9366b8b85c4751ea40e4eb910a76c1a3347a1de5
Semuraii <208602227+Semuraii@users.noreply.github.com>
2026-06-29 11:15:33 +0200

    pokeapi linked

---
+++ script.js
@@ -0,0 +1,6 @@
+const pokemonContainer = document.getElementById("pokemonContainer");
+const pokemonList = document.getElementById("pokemonList");
+const searchBtn = document.getElementById("searchBtn");
+const searchInput = document.getElementById("searchInput");
+
+const BASE_URL = "https://pokeapi.co/api/v2";
