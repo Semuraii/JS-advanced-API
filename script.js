ed3aa53fd41a564c4e6003363245d609a81d0dd3
Semuraii <208602227+Semuraii@users.noreply.github.com>
2026-06-29 11:22:42 +0200

    event listener btn added

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const searchBtn = document.getElementById("searchBtn");
 const searchInput = document.getElementById("searchInput");
 
 const BASE_URL = "https://pokeapi.co/api/v2";
+
+searchBtn.addEventListener("click", () => {
+    const pokemon = searchInput.value.toLowerCase().trim();
+
+    if(pokemon !==""){
+        getPokemon(pokemon);
+    }
+});
