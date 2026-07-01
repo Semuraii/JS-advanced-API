c739afb7a6b360831cef52fd19fb05aa4d321fd7
Semuraii <208602227+Semuraii@users.noreply.github.com>
2026-07-01 10:18:48 +0200

    reset and random added to js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const pokemonContainer = document.getElementById("pokemonContainer");
 const pokemonList = document.getElementById("pokemonList");
 const searchBtn = document.getElementById("searchBtn");
 const searchInput = document.getElementById("searchInput");
+const randomBtn = document.getElementById("randomBtn");
+const resetBtn = document.getElementById("resetBtn");
 
 const BASE_URL = "https://pokeapi.co/api/v2";
 
